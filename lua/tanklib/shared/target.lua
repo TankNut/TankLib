eddd6d74c035de6a636d7b346e56281d7c4dfd7a
TankNut <m.vlasblom@live.nl>
2020-10-03 03:54:37 +0200

    Adds TankLib.Target
    Provides a shared alternative to entity:BodyTarget()

---
+++ lua/tanklib/shared/target.lua
@@ -0,0 +1,40 @@
+local target = {
+	Fallback = function(ent)
+		local low = ent:WorldSpaceCenter() - (ent:WorldSpaceCenter() - ent:GetPos()) * 0.25
+		local high = ent:EyePos()
+
+		local delta = high - low
+
+		return low + delta * 0.5
+	end,
+	Functions = {}
+}
+
+local function headcrab(ent)
+	return ent:GetPos() + Vector(0, 0, 6)
+end
+
+target.Functions["npc_headcrab"] = headcrab
+target.Functions["npc_headcrab_fast"] = headcrab
+target.Functions["npc_headcrab_black"] = headcrab
+
+target.Functions["npc_headcrab"] = headcrab
+target.Functions["npc_headcrab_fast"] = headcrab
+target.Functions["npc_headcrab_black"] = headcrab
+target.Functions["npc_headcrab_poison"] = headcrab
+
+local function antlion(ent)
+	local index = ent:LookupBone("Antlion.Body_Bone")
+	local pos = ent:GetBonePosition(index)
+
+	return pos
+end
+
+target.Functions["npc_antlion"] = antlion
+target.Functions["npc_antlionworker"] = antlion
+
+function target:Get(ent)
+	return self.Functions[ent:GetClass()] and self.Functions[ent:GetClass()](ent) or self.Fallback(ent)
+end
+
+TankLib.Target = target
