1be141168658222a689e0f440c19c9be729d4cd3
TankNut <m.vlasblom@live.nl>
2020-10-24 22:17:40 +0200

    Vector utilities

---
+++ lua/tanklib/shared/vector.lua
@@ -0,0 +1,16 @@
+local vector = FindMetaTable("Vector")
+
+function vector:Approach(target, rate)
+	local diff = target - self
+	local ratio = diff:GetNormalized()
+
+	self.x = math.Approach(self.x, target.x, rate * ratio.x)
+	self.y = math.Approach(self.y, target.y, rate * ratio.y)
+	self.z = math.Approach(self.z, target.z, rate * ratio.z)
+end
+
+function vector:Clamp(min, max)
+	self.x = math.Clamp(self.x, min.x, max.x)
+	self.y = math.Clamp(self.y, min.y, max.y)
+	self.z = math.Clamp(self.z, min.z, max.z)
+end
