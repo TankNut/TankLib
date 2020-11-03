1baba1ed2070724613583f74571753578fa7c76c
TankNut <m.vlasblom@live.nl>
2020-11-03 01:04:16 +0100

    math.ClampedRemap

--- lua/tanklib/shared/math.lua
+++ lua/tanklib/shared/math.lua
@@ -1,3 +1,7 @@
 function math.InRange(val, min, max)
 	return val >= min and val <= max
 end
+
+function math.ClampedRemap(val, inMin, inMax, outMin, outMax)
+	return math.Clamp(math.Remap(val, inMin, inMax, outMin, outMax), outMin, outMax)
+end
